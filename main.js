7b890dfe61f6d235f688d10afbc1039403d687cc
Paul PERNALON <paul.pernalon@centrale-marseille.fr>
2021-10-20 02:17:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const {BrowserWindow, app} = require("electron");
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+    mainWindow.maximize()
+    mainWindow.loadFile('./index.html')
+    mainWindow.show()
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) createWindow()
+})
