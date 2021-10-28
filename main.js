4fb2fcde1450ef9a5a5217fa7db9b0cd3c25d184
ppernalon <paul.pernalon@centrale-marseille.fr>
2021-10-28 11:44:36 +0200

    release 1.0.2

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ const express = require("express")
 
 let mainWindow
 let exApp = express()
-exApp.use(express.static('./resources/app'))
+exApp.use(express.static('./'))
+// exApp.use(express.static('./resources/app'))
 
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: 'favicon.ico'
     })
+    mainWindow.menuBarVisible = false
     mainWindow.maximize()
 
     const server = exApp.listen(0, () => {
